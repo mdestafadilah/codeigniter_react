8f3397a2f23657955dbdb1ca17e34364c1a7cde3
Agung Dirgantara <agungmasda29@gmail.com>
2018-10-25 21:36:09 +0700

    update loader view with twig

--- application/modules/site/controllers/Site.php
+++ application/modules/site/controllers/Site.php
@@ -1,9 +1,17 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 class Site extends CI_Controller
 {
+	function __construct()
+	{
+		parent::__construct();
+		$this->config->set_item('base_template','site/default/views/base_template');
+		$this->config->set_item('content_template','site/default/views/');
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');	
+		// $this->load->view('welcome_message');
+		$this->template->load('welcome');
 	}
 }
 
